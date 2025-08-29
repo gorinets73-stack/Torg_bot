0ced4d83059163c1a4474a47ba2d4ce9f619047f
gorinets73-stack <gorinets73@gmail.com>
2025-08-29 06:05:49 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+config = {
+    # ==== Bybit API (Demo) ====
+    "API_KEY": "iiX4VE3pKkwIzN7MW7",
+    "API_SECRET": "7eauCqefE7EPSr5p8esMAyWEJczL5i9uhsLL",
+    "BASE_URL": "https://api-demo.bybit.com",  # для демо-торговли
+
+    # ==== Telegram Bot ====
+    "TELEGRAM_BOT_TOKEN": "8432592746:AAEg3uaH-Xa6tf-pqZI_RjiKaRZAQfsHbXM",
+    "TELEGRAM_CHAT_ID": "1623720732",
+}
