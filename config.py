247d441ea459bda4023d6e98a683ed270d35b019
gorinets73-stack <gorinets73@gmail.com>
2025-09-10 06:01:14 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+config = {
+    # ==== Bitget API (REAL) ====
+    "API_KEY": "iiX4VE3pKkwIzN7MW7",
+    "API_SECRET": "7eauCqefE7EPSr5p8esMAyWEJczL5i9uhsLL",
+    "API_PASSPHRASE": "20220103",
+    "BASE_URL": "https://api.bitget.com",  # Реальный рынок
+
+    # ==== Telegram Bot ====
+    "TELEGRAM_BOT_TOKEN": "8432592746:AAEg3uaH-Xa6tf-pqZI_RjiKaRZAQfsHbXM",
+    "TELEGRAM_CHAT_ID": "1623720732",
+}
